145e1a468d65cc7561ea8bcdb4e7379ebff63805
Reid Naaykens <reidnaaykens@Carlys-MacBook-Air.local>
2019-09-12 21:58:13 -0700

    quick change to the link for 'logout'

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <div className="outside-controls">
           <p>Optimization Feature</p>
-          <a href="/"><span className="arrow"></span>Logout</a>
+          <a href="#"><span className="arrow"></span>Logout</a>
         </div>
       <OptimizationFeature />
     </div>
